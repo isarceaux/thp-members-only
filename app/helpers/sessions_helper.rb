b1ee74f4824c60126df6ab2488c0bdd6b66b11e0
isarceaux <isarceaux>
2017-08-02 18:57:01 +0200

    Remember feature added

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,11 +13,28 @@ module SessionsHelper
     end
   end
 
+  def remember(user)
+    #use the User method remember
+    user.remember
+    #create the 2 cookies one for id and one with the remember_token
+    cookies.permanent[:remember_token] = user.remember_token
+    cookies.permanent.signed[:id] = user.id
+  end
+
+  def forget(user)
+    #use the User method forget
+    user.forget
+    #delete the 2 cookies
+    cookies.delete(:id)
+    cookies.delete(:remember_token)
+  end
+
   def logged_in?
     !current_user.nil?
   end
 
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
